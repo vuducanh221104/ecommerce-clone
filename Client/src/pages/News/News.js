0a9ea6da5300a1d1693d2eb8e29b5d7bd68d1d34
vuducanh221104 <vuducanh22112004@gmail.com>
2024-04-10 23:31:04 +0700

    merge :build

--- Client/src/pages/News/News.js
+++ Client/src/pages/News/News.js
@@ -36,13 +36,13 @@ function News() {
         setVisibleCount((prevCount) => prevCount + 5);
     };
 
-    if (!data) {
-        return (
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className={cx('not-news')}>
-                <h2 className={cx('title')}>Không có bài báo nào !!</h2>
-            </div>
-        );
-    }
+    // if (!data) {
+    //     return (
+    //         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className={cx('not-news')}>
+    //             <h2 className={cx('title')}>Không có bài báo nào !!</h2>
+    //         </div>
+    //     );
+    // }
 
     return (
         <div className={cx('wrapper')}>
